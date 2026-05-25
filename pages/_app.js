3bf08dc4fb0ef4fea218712372486b6b74bb6098
nuxmeg-max <nuxmeg@gmail.com>
2026-05-25 12:47:05 +0700

    Include Font Awesome in the app component
    Add Font Awesome stylesheet to the app component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
